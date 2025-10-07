b79db1fbb25c1ffbaf3b3387d1dc52bae19409ec
MickyGitt444 <workvcgit@gmail.com>
2025-10-07 16:44:09 +0200

    last 6 advanced pages

---
+++ app/src/main/java/com/poe/whosthatmon/PokedexEntry.kt
@@ -0,0 +1,20 @@
+package com.poe.whosthatmon
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PokedexEntry : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_pokedex_entry)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
